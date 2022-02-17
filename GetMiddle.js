126e0d2fd983cbbd319e0c39ffb81c5a32d8d575
abbos0959 <abbosakromovich777@gmail.com>
2022-02-18 00:37:39 +0500

    GetMiddle

---
+++ GetMiddle.js
@@ -0,0 +1,15 @@
+const GetMiddle = (s) => {
+  let position;
+  let soni;
+
+  if (s.length % 2 == 1) {
+    position = Math.floor( s.length / 2);
+    soni = 1;
+  } else {
+    position = s.length / 2 - 1;
+    soni = 2;
+  }
+
+  console.log(s.substring(position, position + soni),s.length/2);
+};
+GetMiddle("yibsnfd");
--- reverse.js
+++ reverse.js
@@ -1,11 +1,37 @@
-const reverse=(n)=>{
-    let bush=[]
-for(let i=1;i<n;i++){
-bush.push(i)
+// const reverse=(n)=>{
+//     let bush=[]
+// for(let i=1;i<n;i++){
+// bush.push(i)
 
-}
-console.log(bush.reverse());
+// }
+// console.log(bush.reverse());
 
-}
+// }
 
-reverse(5)
+// reverse(5)
+
+// function finalGrade(exam, projects) {
+//   if (exam > 90 || projects > 10) {
+//     console.log(100);
+//   } else if ((exam < 90 && exam > 75) || projects > 5) {
+//     console.log(90);
+//   } else if (exam < 75 && exam > 50 && projects > 2) {
+//     console.log(75);
+//   } else if (exam < 50 && projects > 10) {
+//     console.log(100);
+//   } else {
+//     console.log(0);
+//   }
+// }
+// finalGrade(47, 8);
+const salom = (c) => {
+  let bush = [];
+  for (i = 1; i < c; i++) {
+    if (c % i == 0) {
+        bush.push(i)
+      
+    }
+  }
+  console.log(bush);
+};
+salom(12);
