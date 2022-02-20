e7f34c049903e20fb5c81f5b32dfba51f6b5a149
abbos0959 <abbosakromovich777@gmail.com>
2022-02-20 22:31:39 +0500

    reverseWords

---
+++ reverseWords.js
@@ -0,0 +1,7 @@
+const reverseWords=(string)=>{
+//   let a=  string.split("").reverse().join("").split(" ").reverse().join(" ")
+// console.log(a);
+
+console.log(string.split("").reverse().join("").split(" ").reverse().join(" "));
+}
+reverseWords("salom dunyo")
