001f29ef09871fa87b0211a4f3802205a3081638
abbos0959 <abbosakromovich777@gmail.com>
2022-03-24 22:52:14 +0500

    swap

---
+++ cyu6/swap.js
@@ -0,0 +1,8 @@
+const swap=(n)=>{
+    let abbos =n.split("")
+    // console.log(abbos);
+     salom=abbos.map((value)=>{ return value===value.toUpperCase() ? value.toLowerCase() : value.toUpperCase()}).join("")
+    console.log(salom);
+
+}
+swap('CodeWars')
